7ff93c21ad5af1767592999ff4c74d286eeb3bc1
luVbreda <mascarenhas-lblm@hotmail.com>
2023-10-18 11:25:54 -0300

    Update 18/10/2023

---
+++ Unidade-II/Exercicio-5/src/aplicacao/App.java
@@ -0,0 +1,49 @@
+package aplicacao;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.File;
+import java.io.FileWriter;
+
+import entidades.Produto;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        ArrayList<Produto> lista = new ArrayList<>();
+
+        System.out.printf("Digite o caminho do arquivo: "); String caminho = input.nextLine();
+        File arquivo = new File(caminho);
+
+        try (BufferedReader bc = new BufferedReader(new FileReader(arquivo))) {
+            String linha = bc.readLine();
+            System.out.printf("Digite o caminho onde deseja criar o resumo: "); String resposta = input.nextLine();
+            boolean cria = new File(resposta + "\\resposta").mkdir();
+            resposta += "\\resposta\\resumo.csv";
+            System.out.println("Arquivo criado com sucesso: "+cria);
+
+            while(linha != null) {
+                String[] campos = linha.split(",");
+                lista.add(new Produto(campos[0], Double.parseDouble(campos[1]), Integer.parseInt(campos[2])));
+                try (BufferedWriter br = new BufferedWriter(new FileWriter(resposta))){
+                    for(Produto item: lista){
+                        br.write(item.getNome()+","+String.format("%.2f", item.getValor()*item.getQuantidade()));
+                        br.newLine();
+                    }
+                } catch(IOException e){
+                    System.out.println("Erro: "+e.getMessage());
+                }
+
+                linha = bc.readLine();
+            }
+
+        } catch(IOException e){
+            System.out.println("Erro: "+e.getMessage());
+        }
+        input.close();
+    }
+}
